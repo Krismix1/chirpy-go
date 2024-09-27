981a00981426eac0e0eb38549383dd20d3e36f04
Cristian Betivu <cbe@languagewire.com>
2024-09-27 17:36:56 +0300

    Healthz endpoint

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import "net/http"
 
+func healthz(wr http.ResponseWriter, request *http.Request) {
+	wr.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	wr.Write([]byte("OK"))
+}
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/healthz", http.HandlerFunc(healthz))
 
 	server := http.Server{
 		Addr:    ":8080",
